c1db469669ac81b7746314f0633a2acf6a3ff3e7
ankit14ap <ankitp@trulymadly.com>
2018-07-17 10:55:55 +0530

    Memoizatoin approach

--- IdeaProjects/GeeksForGeeks/src/BytelandianGoldCoins.java
+++ IdeaProjects/GeeksForGeeks/src/BytelandianGoldCoins.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 /*In Byteland they have a very strange monetary system.
@@ -10,17 +11,21 @@ You have one gold coin. What is the maximum amount of American dollars you can g
 
 */
 public class BytelandianGoldCoins {
+
+    public static HashMap<Integer, Integer> map = new HashMap<>();
+
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
+        map.put(0, 0);
+        map.put(1, 1);
+        Scanner s = new Scanner(System.in);
         int n;
-        while (s.hasNext()){
-            n=s.nextInt();
-            System.out.println(maxAmericanDollars(n));
+        while (s.hasNext()) {
+            n = s.nextInt();
+            System.out.println(maxAmericanDollars1(n));
         }
-
-
     }
 
+/*
     private static int maxAmericanDollars(int n) {
         if (n < 0)
             return -1;
@@ -28,14 +33,35 @@ public class BytelandianGoldCoins {
             return 0;
         if (n == 1)
             return 1;
-        int arr[] = new int[n + 1];
-        arr[0]=0;
-        arr[1]=1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 0);
+        map.put(1, 1);
         for (int i = 2; i <= n; i++) {
-            arr[i] = arr[i / 2] + arr[i / 3] + arr[i / 4];
-            if(arr[i]<i)
-                arr[i]=i;
+            int t = map.get(i / 2) + map.get(i / 3) + map.get(i / 4);
+            if (t < i)
+                t = i;
+            map.put(i, t);
         }
-        return arr[n];
+        return map.get(n);
     }
+*/
+
+    private static int maxAmericanDollars1(int n) {
+        if (n < 0)
+            return -1;
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if(map.containsKey(n)){
+           return map.get(n);
+        }else {
+            int t=maxAmericanDollars1(n/2)+maxAmericanDollars1(n/3)+maxAmericanDollars1(n/4);
+            if (t < n)
+                t = n;
+            map.put(n,t);
+        }
+        return map.get(n);
+    }
+
 }
